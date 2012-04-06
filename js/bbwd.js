a675189a49397590d55c0faecda20e989cd7c8be
Kevin Rocci <kevinrocci@gmail.com>
2012-04-06 12:58:07 -0700

    expand JSON functionality to anchor attributes

--- js/bbwd.js
+++ js/bbwd.js
@@ -5,6 +5,13 @@ $(document).ready(function() {
         if (key == "title") {
           $('h1.' + clientId).html(val);
         }
+        if (key == "url") {
+          $('a.' + clientId).attr("href" , val);
+		  $('a.' + clientId).attr("target" , "_blank");
+	    }
+        if (key == "linkText") {
+          $('a.' + clientId).html(val);
+        }
         if (key == "description") {
           $('p.' + clientId).html(val);
         }
